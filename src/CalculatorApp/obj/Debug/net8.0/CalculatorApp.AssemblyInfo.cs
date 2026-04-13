df364c292be8acfb112c0b89b3ee82c7e398632a
K Shravani <k.shravani@prodevans.com>
2026-04-13 12:00:04 +0530

    added plugin

--- src/CalculatorApp/obj/Debug/net8.0/CalculatorApp.AssemblyInfo.cs
+++ src/CalculatorApp/obj/Debug/net8.0/CalculatorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83a7c1050516895660c50a045a61e93c259f8146")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
